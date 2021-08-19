5ff7832144704cadf84c654642611de3ccfedbfa
Khairun Nashirin <k.nashirin1910@gmail.com>
2021-08-19 19:57:04 +0700

    setup navigation component and bottom navigation menu

--- app/src/main/java/com/androiddevs/runningappyt/ui/activities/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/activities/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.androiddevs.runningappyt.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
-import com.androiddevs.runningappyt.database.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.runFragment, R.id.statisticsFragment, R.id.settingsFragment -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                else -> bottomNavigationView.visibility = View.GONE
+            }
+        }
     }
 }
