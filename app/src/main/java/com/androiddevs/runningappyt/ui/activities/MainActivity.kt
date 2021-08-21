af6a229ea294b14c180349ce2ed20c356e0a73de
Khairun Nashirin <k.nashirin1910@gmail.com>
2021-08-21 07:49:05 +0700

    create foreground service with notification

--- app/src/main/java/com/androiddevs/runningappyt/ui/activities/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/activities/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.androiddevs.runningappyt.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
+import com.androiddevs.runningappyt.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Main activity is destroyed before, when clicked foreground service notification
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
 
@@ -27,6 +32,19 @@ class MainActivity : AppCompatActivity() {
                 }
                 else -> bottomNavigationView.visibility = View.GONE
             }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        // Main activity is not destroyed before, when clicked foreground service notification
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
         }
     }
 }
